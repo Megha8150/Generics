9e43a90b9171f0f224dac0fe60d5cc4b16ec9342
Megha8150 <meghananayak436@gmail.com>
2022-07-20 23:57:14 +0530

    Checked Maximum int number

--- Generics/Program.cs
+++ Generics/Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Generics");
+
+using Generics;
+Console.WriteLine("test case 1");
+Console.WriteLine("Maximum Integer Number is " +MaxInt.MaximumIntegerNumber(10,1,5));
+Console.WriteLine("test case 2");
+Console.WriteLine("Maximum Integer Number is " +MaxInt.MaximumIntegerNumber(3, 15, 9));
+Console.WriteLine("test case 3");
+Console.WriteLine("Maximum Integer Number is " +MaxInt.MaximumIntegerNumber(5, 8, 12));
+
